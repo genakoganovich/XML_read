c7a4d950c6578b2bd2eb64db4e66bc32fa1d6cc4
Gennadiy Koganovich <genakoganovich@gmail.com>
2023-03-03 20:42:08 +0200

    0006: replace middle white spaces with string.ljust(column_width)

--- text_header.py
+++ text_header.py
@@ -5,25 +5,36 @@ def print_xml():
     line_parts = []
     with minidom.parse('input/template.xml') as file:
         line_length = int(file.getElementsByTagName('line_length')[0].childNodes[0].data)
+        column_width = int(file.getElementsByTagName('column_width')[0].childNodes[0].data)
         count = 1
 
         for line in file.getElementsByTagName('line'):
             line_parts.append('C{0:2d}  '.format(count))
 
             for part in line.getElementsByTagName('part'):
-                line_parts.append(select_part(part, part.getElementsByTagName('q')))
+                line_parts.append(get_data_to_append(column_width, line_parts, part))
 
             print("".join(line_parts).ljust(line_length))
             count += 1
             line_parts.clear()
 
 
-def select_part(part, q):
-    if q:
-        return ' ' * int(q[0].childNodes[0].data)
+def get_data_to_append(column_width, line_parts, part):
+    if part.getAttribute("type") == 'c':
+        to_append = (line_parts.pop() + part.childNodes[0].data).ljust(column_width)
     else:
-        return part.childNodes[0].data
+        to_append = part.childNodes[0].data
+    return to_append
+
+
+def get_attr():
+    with minidom.parse('input/template.xml') as file:
+        line = file.getElementsByTagName('line')[0]
+        part = line.getElementsByTagName('part')[0]
+        print(part.getAttribute("type"))
 
 
 if __name__ == '__main__':
     print_xml()
+    # get_attr()
+
