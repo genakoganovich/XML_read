0bcb34652afaed387074b858b9b47176e5a31c20
Gennadiy Koganovich <genakoganovich@gmail.com>
2023-03-03 17:14:41 +0200

    0003: replace white spaces with <q>

--- text_header.py
+++ text_header.py
@@ -1,14 +1,26 @@
 from xml.dom import minidom
 
-# parse a xml file by name
-with minidom.parse('input/template.xml') as file:
-    count = 1
 
-    for line in file.getElementsByTagName('line'):
-        print('C{0:2d}  '.format(count), end='')
+def print_xml():
+    with minidom.parse('input/template.xml') as file:
+        count = 1
 
-        for part in line.getElementsByTagName('text'):
-            print(part.childNodes[0].data, end='')
+        for line in file.getElementsByTagName('line'):
+            print('C{0:2d}  '.format(count), end='')
 
-        print()
-        count += 1
+            for part in line.getElementsByTagName('part'):
+                print_part(part, part.getElementsByTagName('q'))
+
+            print()
+            count += 1
+
+
+def print_part(part, q):
+    if q:
+        print(' ' * int(q[0].childNodes[0].data), end='')
+    else:
+        print(part.childNodes[0].data, end='')
+
+
+if __name__ == '__main__':
+    print_xml()
