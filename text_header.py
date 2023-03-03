b80128e93fdbb0967322266c26e6b5f51b616d36
Gennadiy Koganovich <genakoganovich@gmail.com>
2023-03-03 18:52:02 +0200

    0005: replace ending white spaces with string.ljust(line_length)

--- text_header.py
+++ text_header.py
@@ -2,17 +2,20 @@ from xml.dom import minidom
 
 
 def print_xml():
+    line_parts = []
     with minidom.parse('input/template.xml') as file:
+        line_length = int(file.getElementsByTagName('line_length')[0].childNodes[0].data)
         count = 1
 
         for line in file.getElementsByTagName('line'):
-            print('C{0:2d}  '.format(count), end='')
+            line_parts.append('C{0:2d}  '.format(count))
 
             for part in line.getElementsByTagName('part'):
-                print(select_part(part, part.getElementsByTagName('q')), end='')
+                line_parts.append(select_part(part, part.getElementsByTagName('q')))
 
-            print()
+            print("".join(line_parts).ljust(line_length))
             count += 1
+            line_parts.clear()
 
 
 def select_part(part, q):
