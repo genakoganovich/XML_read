d75b40707ea92f646425002e6177daadeaf96c73
Gennadiy Koganovich <genakoganovich@gmail.com>
2023-03-03 22:53:05 +0200

    0007: add left and right column width

--- text_header.py
+++ text_header.py
@@ -5,26 +5,29 @@ def print_xml():
     line_parts = []
     with minidom.parse('input/template.xml') as file:
         line_length = int(file.getElementsByTagName('line_length')[0].childNodes[0].data)
-        column_width = int(file.getElementsByTagName('column_width')[0].childNodes[0].data)
+        left_width = int(file.getElementsByTagName('left_width')[0].childNodes[0].data)
+        right_width = int(file.getElementsByTagName('right_width')[0].childNodes[0].data)
+
         count = 1
 
         for line in file.getElementsByTagName('line'):
             line_parts.append('C{0:2d}  '.format(count))
 
             for part in line.getElementsByTagName('part'):
-                line_parts.append(get_data_to_append(column_width, line_parts, part))
+                line_parts.append(get_data_to_append(line_parts, part, left_width, right_width))
 
             print("".join(line_parts).ljust(line_length))
             count += 1
             line_parts.clear()
 
 
-def get_data_to_append(column_width, line_parts, part):
-    if part.getAttribute("type") == 'c':
-        to_append = (line_parts.pop() + part.childNodes[0].data).ljust(column_width)
+def get_data_to_append(line_parts, part, left_width, right_width):
+    if part.getAttribute("column") == "left":
+        return (line_parts.pop() + part.childNodes[0].data).ljust(left_width)
+    elif part.getAttribute("column") == "right":
+        return (line_parts.pop() + part.childNodes[0].data).ljust(right_width)
     else:
-        to_append = part.childNodes[0].data
-    return to_append
+        return part.childNodes[0].data
 
 
 def get_attr():
@@ -37,4 +40,3 @@ def get_attr():
 if __name__ == '__main__':
     print_xml()
     # get_attr()
-
