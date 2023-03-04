d6ebebd4e4055888b07c84a4bc03e791493ba5cb
Gennadiy Koganovich <genakoganovich@gmail.com>
2023-03-04 10:05:15 +0200

    0008: remove right column width, add info attr, remove popping, replace ljust with whitespace adding

--- text_header.py
+++ text_header.py
@@ -4,9 +4,8 @@ from xml.dom import minidom
 def print_xml():
     line_parts = []
     with minidom.parse('input/template.xml') as file:
-        line_length = int(file.getElementsByTagName('line_length')[0].childNodes[0].data)
+        total_width = int(file.getElementsByTagName('total_width')[0].childNodes[0].data)
         left_width = int(file.getElementsByTagName('left_width')[0].childNodes[0].data)
-        right_width = int(file.getElementsByTagName('right_width')[0].childNodes[0].data)
 
         count = 1
 
@@ -14,29 +13,28 @@ def print_xml():
             line_parts.append('C{0:2d}  '.format(count))
 
             for part in line.getElementsByTagName('part'):
-                line_parts.append(get_data_to_append(line_parts, part, left_width, right_width))
+                line_parts.append(get_data_to_append(line_parts, part, left_width))
 
-            print("".join(line_parts).ljust(line_length))
+            print("".join(line_parts).ljust(total_width))
             count += 1
             line_parts.clear()
 
 
-def get_data_to_append(line_parts, part, left_width, right_width):
-    if part.getAttribute("column") == "left":
-        return (line_parts.pop() + part.childNodes[0].data).ljust(left_width)
-    elif part.getAttribute("column") == "right":
-        return (line_parts.pop() + part.childNodes[0].data).ljust(right_width)
+def get_data_to_append(line_parts, part, left_width):
+    if part.hasAttribute("info"):
+        return ''
+    elif part.getAttribute("column") == "left":
+        return ' ' * (left_width - sum(map(len, line_parts)))
     else:
         return part.childNodes[0].data
 
 
-def get_attr():
+def test():
     with minidom.parse('input/template.xml') as file:
         line = file.getElementsByTagName('line')[0]
-        part = line.getElementsByTagName('part')[0]
-        print(part.getAttribute("type"))
+        print(len(line.childNodes))
 
 
 if __name__ == '__main__':
     print_xml()
-    # get_attr()
+    # test()
